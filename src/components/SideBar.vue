81e927f311b7b6f687935f85a793cce4b508a93c
SamiRZL <102523018+SamiRZL@users.noreply.github.com>
2024-05-11 10:56:52 +0100

    fixing files

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -5,7 +5,7 @@
                 <path fill="#202020"
                     d="M96 128a128 128 0 1 0 256 0A128 128 0 1 0 96 128zm94.5 200.2l18.6 31L175.8 483.1l-36-146.9c-2-8.1-9.8-13.4-17.9-11.3C51.9 342.4 0 405.8 0 481.3c0 17 13.8 30.7 30.7 30.7H162.5c0 0 0 0 .1 0H168 280h5.5c0 0 0 0 .1 0H417.3c17 0 30.7-13.8 30.7-30.7c0-75.5-51.9-138.9-121.9-156.4c-8.1-2-15.9 3.3-17.9 11.3l-36 146.9L238.9 359.2l18.6-31c6.4-10.7-1.3-24.2-13.7-24.2H224 204.3c-12.4 0-20.1 13.6-13.7 24.2z" />
             </svg>
-            <h1>{{ this.Store.user.firstname }} {{ this.Store.user.firstname }}</h1>
+            <h1>{{ this.firstname }} {{ this.lastname }}</h1>
         </div>
         <ul>
             <router-link to="/documents">
@@ -69,7 +69,9 @@ export default {
         getUser() {
             axios.get(`https://sika-info-server.vercel.app/users/loggedUser`, { withCredentials: true })
                 .then((response) => {
-                    console.log("user fetched", response.data)
+                    console.log("user fetched", response.data);
+                    this.firstname = response.data.user.firstname;
+                    this.lastname = response.data.user.lastname;
                 })
                 .catch((error) => { console.log("error logging user", error); this.loading = false; })
         },
